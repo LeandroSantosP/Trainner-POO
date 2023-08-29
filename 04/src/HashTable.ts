6487069214ad73b08dbe43e2577a0fc2f50ed707
LeandroSantosP <skp.Tridimensional@gmail.com>
2023-08-29 18:27:30 -0300

    create get and set for hashTable

--- 04/src/HashTable.ts
+++ 04/src/HashTable.ts
@@ -1,16 +1,34 @@
+class Table {
+  private value: string;
+  constructor(value: string) {
+    this.value = value;
+  }
+  getValue() {
+    return this.value;
+  }
+}
 export class HashTable {
   size: number;
-  table: any[];
+  table: Table[];
   constructor(tableSize: number = 128) {
     this.table = new Array(tableSize);
     this.size = 0;
   }
-
-  hash(key: string) {
+  _hash(key: string) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash += key.charCodeAt(i);
     }
-    return hash;
+    return hash % this.table.length;
+  }
+  set(key: string, value: string): void {
+    const hashIndex = this._hash(key);
+    this.table[hashIndex] = new Table(value);
+    this.size++;
+  }
+  get(key: string) {
+    const index = this._hash(key);
+    if (!this.table[index]) return;
+    return this.table[index].getValue();
   }
 }
--- 04/test/HashTable.test.ts
+++ 04/test/HashTable.test.ts
@@ -1,9 +1,24 @@
 import { HashTable } from "../src/HashTable";
 
 test("Deve criar uma hash a partir de uma key!", function () {
-  const hashTable = new HashTable();
+  const hashTable = new HashTable(100);
 
-  const hash = hashTable.hash("name");
+  const hash = hashTable._hash("name");
 
-  expect(hash).toBe(417);
+  expect(hash).toBe(17);
+  expect(hash).toBeLessThan(100);
+  expect(hash).toBeGreaterThan(0);
+});
+
+test("Deve setar um item na hashTable utilizando o metade set e obtelo utilizando o metado get.", function () {
+  const hashTable = new HashTable(100);
+
+  hashTable.set("name", "johnDoe");
+
+  const item = hashTable.get("name");
+  const item2 = hashTable.get("invalid");
+
+  expect(item2).toBeUndefined();
+  expect(item).toBe("johnDoe");
+  expect(hashTable.size).toBe(1);
 });
