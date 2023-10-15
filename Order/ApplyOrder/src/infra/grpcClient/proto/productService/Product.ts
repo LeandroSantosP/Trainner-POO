fbb5823bc53ad48d1be0ae08a2b62b5bbcd27f7c
LeandroSantosP <skp.tridimensional@gmail.com>
2023-10-15 19:14:38 -0300

    start create grpc client on applyOrder aplicatio

---
+++ Order/ApplyOrder/src/infra/grpcClient/proto/productService/Product.ts
@@ -0,0 +1,22 @@
+// Original file: src/infra/grpcClient/proto/productService.proto
+
+
+export interface Product {
+  'productId'?: (string);
+  'price'?: (number | string);
+  'name'?: (string);
+  'weight'?: (number | string);
+  'density'?: (number | string);
+  'volume'?: (number | string);
+  'fare'?: (number | string);
+}
+
+export interface Product__Output {
+  'productId'?: (string);
+  'price'?: (number);
+  'name'?: (string);
+  'weight'?: (number);
+  'density'?: (number);
+  'volume'?: (number);
+  'fare'?: (number);
+}
